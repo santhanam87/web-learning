fea5ec1e4d70392eaaac5b8750a60011cb8e690d
shravansudo <shravansanthanam@gmail.com>
2025-02-25 18:36:13 -0700

    Add explorer.js file
    Some random functionality.

---
+++ explorer.js
@@ -0,0 +1,15 @@
+  var game = (Math.random(566)*909.90)
+  console.info (game)
+
+  function mathGame (min,max){
+      return (Math.random(min)*max)
+  }
+  console.info(mathGame(9090,88990))
+
+  var game = (Math.random(566)*909.90)
+  console.info (game)
+
+  function mathGame (min,max){
+      return (Math.random(min)*max)
+  }
+ console.info(mathGame(9090,88990))
