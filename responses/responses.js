0fdd5ddef9e6891beb49b2c9596a452fca5e9a18
shravansudo <shravansanthanam@gmail.com>
2025-03-12 16:21:19 -0600

    I tried to do a while loop but failed

--- responses/responses.js
+++ responses/responses.js
@@ -1,7 +1,42 @@
 //- Write a `for` loop that iterates from 1 to 10 and calculates the sum of all numbers in that range.
 //- Print the final sum to the console.
-var number = 0;
-for(var numIndex = 0;numIndex<11;numIndex++){
-number=number+numIndex
-}
-console.info(number,numIndex,1+2+3+4+5+6+7+8+9+10)
+// var number = 0;
+// for(var numIndex = 0;numIndex<11;numIndex++){
+// number=number+numIndex
+// }
+//console.info(number,numIndex,1+2+3+4+5+6+7+8+9+10)
+
+// - Write a `for` loop that iterates from 1 to 10.
+// - Use a `break` statement to exit the loop when the number 5 is reached.
+// - Use a `continue` statement to skip the number 3.
+// - Print the numbers that are processed by the loop.
+// for(var numIndex1=0;numIndex1<11;numIndex1++){
+//     if (numIndex1==3) {
+//         continue
+//     }
+//     else if(numIndex1==5){
+//         break
+//     }
+//     console.info(numIndex1)
+
+// }
+// - Use nested `for` loops to create the following pattern:
+// ```
+// *
+// **
+// ***
+// ****
+// *****
+// ```
+
+//var firstName = "santhanam"
+//var lastName = "elumalai"
+//var name = firstName+lastName 
+// for (var patternIndex=1;patternIndex<7;patternIndex++){
+//     var asterisk = "";
+//     for(var asteriskIndex=0;asteriskIndex<patternIndex;asteriskIndex++){
+//     asterisk=asterisk+"*"}
+// console.info(asterisk, patternIndex)
+// }
+//- Write a `while` loop that generates random numbers between 1 and 10 until it generates the number 7.
+//    - Print each generated number to the console, and then print a message when the number 7 is found.   
