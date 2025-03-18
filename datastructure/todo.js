3ef0d9a34ad98696f7dbf25ab876dc6d32fba36b
shravansudo <shravansanthanam@gmail.com>
2025-03-17 18:40:44 -0600

    reviewed my work of the banking task

--- datastructure/todo.js
+++ datastructure/todo.js
@@ -3,7 +3,7 @@
  * Build a datascrture for a banking system, with the following information bankName, managerName, locations (city, state, zip, locationId),
  * employees - employeeName (firstName, LastName), designation, yearOfExp, locationId
  */
-var Bank_Info = [
+var banks = [
   {
     bankName: "Best bank",
     managerName: "Joe",
@@ -13,6 +13,14 @@ var Bank_Info = [
       zip: "0986746",
       locationId: "6586824",
     },
+    employes: [
+      {
+        name: { firstName: "Jon", lastName: "Dow" },
+        designation: "janetor",
+        yearOfExp: "5",
+        locationId: "6586824",
+      },
+    ],
   },
   {
     bankName: "Best bank",
@@ -23,23 +31,17 @@ var Bank_Info = [
       zip: "0977461",
       locationId: "6678624",
     },
+    employes: [
+      {
+        name: { firstName: "Jonny", lastName: "Dow" },
+        designation: "teller",
+        yearOfExp: "15",
+        locationId: "6678624",
+      },
+    ],
   },
 ];
-var Employe_info = [
-  {
-    employe: { firstName: "Jon", lastName: "Dow" },
-    designation: "janetor",
-    yearOfExp: "5",
-    locationId: Bank_Info[0].locations.locationId,
-  },
-  {
-    employe: { firstName: "Jonny", lastName: "Dow" },
-    designation: "teller",
-    yearOfExp: "15",
-    locationId: Bank_Info[1].locations.locationId,
-  },
-];
-console.info(Bank_Info, Employe_info);
+console.info(JSON.stringify(banks));
 // TODO: Narato
 //WHY NARRATO WHY???!!!
 /**
